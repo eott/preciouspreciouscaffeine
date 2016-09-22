80dd90d859f786c6b2e05cc66e201f5f0329550a
Etienne Ott <eti.ott@gmail.com>
2016-09-22 19:51:55 +0200

    Add items for berries, beans, roasted beans, powder and mugs.

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,6 +1,6 @@
 data:extend({
 
- {
+  {
     type = "item",
     name = "coffee-plantation",
     icon = "__PreciousPreciousCaffeine__/graphics/icon_coffee_plantation.png",
@@ -8,6 +8,56 @@ data:extend({
     subgroup = "extraction-machine",
     place_result="coffee-plantation",
     stack_size= 50,
+  },
+
+  {
+    type = "item",
+    name = "coffee-berries",
+    icon = "__PreciousPreciousCaffeine__/graphics/icon_coffee_berries.png",
+    flags = { "goes-to-main-inventory" },
+    subgroup = "raw-material",
+    place_result="coffee-berries",
+    stack_size= 200,
+  },
+
+  {
+    type = "item",
+    name = "coffee-beans",
+    icon = "__PreciousPreciousCaffeine__/graphics/icon_coffee_beans.png",
+    flags = { "goes-to-main-inventory" },
+    subgroup = "intermediate-product",
+    place_result="coffee-beans",
+    stack_size= 200,
+  },
+
+  {
+    type = "item",
+    name = "roasted-coffee-beans",
+    icon = "__PreciousPreciousCaffeine__/graphics/icon_roasted_coffee_beans.png",
+    flags = { "goes-to-main-inventory" },
+    subgroup = "intermediate-product",
+    place_result="roasted-coffee-beans",
+    stack_size= 200,
+  },
+
+  {
+    type = "item",
+    name = "coffee-powder",
+    icon = "__PreciousPreciousCaffeine__/graphics/icon_coffee_powder.png",
+    flags = { "goes-to-main-inventory" },
+    subgroup = "intermediate-product",
+    place_result="coffee-powder",
+    stack_size= 50,
+  },
+
+  {
+    type = "item",
+    name = "mug-of-coffee",
+    icon = "__PreciousPreciousCaffeine__/graphics/icon_mug_of_coffee.png",
+    flags = { "goes-to-main-inventory" },
+    subgroup = "barrel",
+    place_result="mug-of-coffee",
+    stack_size= 100,
   }
 
 })
