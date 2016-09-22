cf26ec79a713b8a43d86c9e88066efef211cfcd3
Etienne Ott <eti.ott@gmail.com>
2016-09-22 19:58:41 +0200

    Add recipes for coffee products.

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -1,5 +1,5 @@
 data:extend({
- {
+  {
     type = "recipe",
     name = "coffee-plantation",
     enabled = "true",
@@ -11,5 +11,56 @@ data:extend({
       {"inserter",2}
     },
     result = "coffee-plantation"
+  },
+
+  {
+    type = "recipe",
+    name = "coffee-beans",
+    enabled = "true",
+    ingredients = 
+    {
+      {"coffee-berries",1}
+    },
+    result = "coffee-beans"
+  },
+  
+  {
+    type = "recipe",
+    name = "roasted-coffee-beans",
+    enabled = "true",
+    category = "smelting",
+    energy_required = 3.5,
+    ingredients = 
+    {
+      {"coffee-beans",1}
+    },
+    result = "roasted-coffee-beans"
+  },
+
+  {
+    type = "recipe",
+    name = "coffee-powder",
+    enabled = "true",
+    ingredients = 
+    {
+      {"roasted-coffee-beans",20}
+    },
+    result = "coffee-powder"
+  },
+
+  {
+    type = "recipe",
+    name = "mug-of-coffee",
+    enabled = "true",
+    energy_required = 3,
+    category = "crafting-with-fluid",
+    ingredients = 
+    {
+      {"roasted-coffee-beans",20},
+      {type="fluid", name="water", amount=2.5},
+      {"stone", 5}
+    },
+    result = "mug-of-coffee"
+    result_count = 5
   },
 })
