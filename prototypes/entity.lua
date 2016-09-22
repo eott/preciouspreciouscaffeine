4052545be8503a339087f671593e19a6b526c2d6
Etienne Ott <eti.ott@gmail.com>
2016-09-22 19:37:19 +0200

    Change image assets for coffee plantation.

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -7,8 +7,8 @@ data:extend({
     minable = {mining_time = 1, result = "coffee-plantation"},
     max_health = 150,
     corpse = "medium-remnants",
-    collision_box = {{ -0.9, -0.9}, {0.9, 0.9}},
-    selection_box = {{ -1.0, -1.0}, {1.0, 1.0}},
+    collision_box = {{-1.4, -1.4}, {1.4, 1.4}},
+    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
     inventory_size = 1,
     energy_source =
     {
@@ -20,10 +20,10 @@ data:extend({
     render_layer = "object",
     picture = 
     {
-    filename = "__PreciousPreciousCaffeine__/graphics/coffee_plantation.png",
-    width = 64,
-    height = 64,
-    shift = {0.95, 0.2}
+      filename = "__PreciousPreciousCaffeine__/graphics/coffee_plantation.png",
+      width = 208,
+      height = 115,
+      shift = {1.35, 0.0}
     },
     energy_usage = "10kW",
     module_specification =
