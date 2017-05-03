b720b3e7c528f65f95e463025f316244137ad667
Etienne Ott <eti.ott@gmail.com>
2017-05-03 19:17:18 +0200

    Remove subgroup of mug of coffee item as it no longer exists.

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -51,7 +51,6 @@ data:extend({
     name = "mug-of-coffee",
     icon = "__PreciousPreciousCaffeine__/graphics/icon_mug_of_coffee.png",
     flags = { "goes-to-main-inventory" },
-    subgroup = "barrel",
     stack_size= 100,
   }
 
