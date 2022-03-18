eb2ca2be797c93153109fc50495992281b2c2774
Etienne Ott <eti.ott@gmail.com>
2022-03-18 11:52:29 +0100

    Split availability of items and recipes into four technologies

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -1,8 +1,8 @@
 data:extend({
   {
       type = "technology",
-      name = "ppc-coffee-production",
-      icon = "__PreciousPreciousCaffeine__/graphics/icon_coffee_production.png",
+      name = "ppc-coffee-plantations",
+      icon = "__PreciousPreciousCaffeine__/graphics/tech_coffee_plantations.png",
       icon_size = 128,
       effects =
       {
@@ -14,54 +14,104 @@ data:extend({
             type = "unlock-recipe",
             recipe = "coffee-berries"
         },
-        {
-            type = "unlock-recipe",
-            recipe = "coffee-beans"
-        },
-        {
-            type = "unlock-recipe",
-            recipe = "roasted-coffee-beans"
-        },
-        {
-            type = "unlock-recipe",
-            recipe = "coffee-powder"
-        },
-        {
-            type = "unlock-recipe",
-            recipe = "mug-of-coffee"
-        },
-        {
-            type = "unlock-recipe",
-            recipe = "coffee-crate"
-        },
-        {
-            type = "unlock-recipe",
-            recipe = "cryo-shipment"
-        }
       },
-      prerequisites = {"automation-2"},
+      prerequisites = {},
       unit =
       {
-        count = 30,
+        count = 10,
         ingredients =
         {
           {"automation-science-pack", 1},
-          {"logistic-science-pack", 1},
         },
         time = 20
       }
   },
 
   {
+    type = "technology",
+    name = "ppc-coffee-production",
+    icon = "__PreciousPreciousCaffeine__/graphics/tech_coffee_production.png",
+    icon_size = 128,
+    effects =
+    {
+      {
+          type = "unlock-recipe",
+          recipe = "coffee-beans"
+      },
+      {
+          type = "unlock-recipe",
+          recipe = "roasted-coffee-beans"
+      },
+      {
+          type = "unlock-recipe",
+          recipe = "coffee-powder"
+      },
+      {
+          type = "unlock-recipe",
+          recipe = "mug-of-coffee"
+      },
+    },
+    prerequisites = {
+      "automation-2",
+      "ppc-coffee-plantations",
+    },
+    unit =
+    {
+      count = 30,
+      ingredients =
+      {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+      },
+      time = 20
+    }
+  },
+
+  {
+    type = "technology",
+    name = "ppc-interstellar-coffee",
+    icon = "__PreciousPreciousCaffeine__/graphics/tech_interstellar_coffee.png",
+    icon_size = 128,
+    effects =
+    {
+      {
+          type = "unlock-recipe",
+          recipe = "coffee-crate"
+      },
+      {
+          type = "unlock-recipe",
+          recipe = "cryo-shipment"
+      }
+    },
+    prerequisites = {"rocket-silo"},
+    unit =
+    {
+      count = 100,
+      ingredients =
+      {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"production-science-pack", 1},
+        {"space-science-pack", 1},
+      },
+      time = 20
+    }
+  },
+
+  {
       type = "technology",
       name = "ppc-auto-consumption",
-      icon = "__PreciousPreciousCaffeine__/graphics/icon_auto_consumption.png",
+      icon = "__PreciousPreciousCaffeine__/graphics/tech_auto_consumption.png",
       icon_size = 128,
       effects =
       {
         
       },
-      prerequisites = {"ppc-coffee-production"},
+      prerequisites = {
+        "ppc-coffee-production",
+        "chemical-science-pack",
+      },
       unit =
       {
         count = 30,
