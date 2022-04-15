4a3e9c00433f682c5ffd0177d8060be8876b8d6f
Etienne Ott <eti.ott@gmail.com>
2022-04-15 12:06:41 +0200

    Fix wrong presets for rocket projectile for 3A rocket

--- prototypes/projectiles.lua
+++ prototypes/projectiles.lua
@@ -34,7 +34,9 @@ data:extend({
     type = "projectile",
     name = "3a-rocket-flying",
     flags = {"not-on-map"},
-    acceleration = 0.05,
+    acceleration = 0.005,
+    turn_speed = 0.003,
+    turning_speed_increases_exponentially_with_projectile_speed = true,
     action =
     {
       type = "direct",
@@ -49,16 +51,41 @@ data:extend({
         }
       }
     },
-    light = {intensity = 0.5, size = 4},
+    --light = {intensity = 0.5, size = 4},
     animation =
     {
       filename = "__base__/graphics/entity/rocket/rocket.png",
-      frame_count = 1,
-      width = 3,
-      height = 50,
+      draw_as_glow = true,
+      frame_count = 8,
+      line_length = 8,
+      width = 9,
+      height = 35,
+      shift = {0, 0},
       priority = "high"
     },
-    smoke = capsule_smoke,
+    shadow =
+    {
+      filename = "__base__/graphics/entity/rocket/rocket-shadow.png",
+      frame_count = 1,
+      width = 7,
+      height = 24,
+      priority = "high",
+      shift = {0, 0}
+    },
+    smoke =
+    {
+      {
+        name = "smoke-fast",
+        deviation = {0.15, 0.15},
+        frequency = 1,
+        position = {0, 1},
+        slow_down_factor = 1,
+        starting_frame = 3,
+        starting_frame_deviation = 5,
+        starting_frame_speed = 0,
+        starting_frame_speed_deviation = 5
+      }
+    }
   },
 
   {
