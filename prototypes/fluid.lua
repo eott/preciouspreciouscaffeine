b15dcac57c40f52b2bf6ab4327a0cbaf88cfb130
NaN <raudorn@gmail.com>
2024-12-31 16:13:27 +0100

    Fix fluids the mod introduced not being categorized correctly.

--- prototypes/fluid.lua
+++ prototypes/fluid.lua
@@ -2,12 +2,12 @@ data:extend({
     {
       type = "fluid",
       name = "fluid-coffee",
+      subgroup = "fluid",
       icon = "__PreciousPreciousCaffeine__/graphics/fluid_coffee.png",
       icon_size = 64,
       icon_mipmaps = 4,
       base_color = {r=0.22, g=0, b=0},
       flow_color = {r=0.93, g=0.055, b=0},
-      subgroup = "coffee-intermediates",
       default_temperature = 20,
       max_temperature = 100,
       order = "z-a",
@@ -18,6 +18,7 @@ data:extend({
     {
       type = "fluid",
       name = "fluid-compressed-air",
+      subgroup = "fluid",
       default_temperature = 20,
       max_temperature = 1000,
       icon = "__PreciousPreciousCaffeine__/graphics/fluid_compressed_air.png",
@@ -32,6 +33,7 @@ data:extend({
     {
       type = "fluid",
       name = "fluid-liquid-co2",
+      subgroup = "fluid",
       default_temperature = 20,
       max_temperature = 60,
       icon = "__PreciousPreciousCaffeine__/graphics/fluid_co2.png",
